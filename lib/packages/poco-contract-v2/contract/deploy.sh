c7856d8d4cd4ba7dd7c623055969f8a12166e1f9
Twiliness <DarkHighness@users.noreply.github.com>
2022-11-24 17:14:02 +0800

    feat: 搭建 NEAR 基本环境

---
+++ lib/packages/poco-contract-v2/contract/deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+./build.sh
+
+if [ $? -ne 0 ]; then
+  echo ">> Error building contract"
+  exit 1
+fi
+
+echo ">> Deploying contract"
+
+if [ $NEAR_ENV = "local" ]; then
+  NEAR_MODE="env"
+  source ../near-env.sh
+  echo ">> Use local near blockchain"
+  NEAR_DEPLOY_COMMAND="pnpm exec near deploy"
+elif [ $NEAR_ENV = "testnet" ]; then
+  echo ">> Use near testnet"
+  NEAR_DEPLOY_COMMAND="pnpm exec near dev-deploy"
+else
+  echo "Unsupport NEAR_ENV : ${NEAR_ENV}"
+  exit 1
+fi
+
+
+# https://docs.near.org/tools/near-cli#near-dev-deploy
+echo "y" | $NEAR_DEPLOY_COMMAND --wasmFile ${WORKSPACE_DIR}/contract/target/wasm32-unknown-unknown/release/poco.wasm --accountId ${NEAR_HELPER_ACCOUNT}
