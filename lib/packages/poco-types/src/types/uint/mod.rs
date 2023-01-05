ea7f405f442b9888e5a458ebf1351fbbc3c3d153
Twiliness <DarkHighness@users.noreply.github.com>
2023-01-05 11:24:46 +0800

    feat: 为 U256 实现序列化

--- lib/packages/poco-types/src/types/uint/mod.rs
+++ lib/packages/poco-types/src/types/uint/mod.rs
@@ -1,10 +1,13 @@
+use impl_serde::impl_uint_serde;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::serde::{Deserialize, Serialize};
 use schemars::JsonSchema;
 use uint::construct_uint;
 
 construct_uint! {
-    #[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, JsonSchema)]
+    #[derive(BorshDeserialize, BorshSerialize, JsonSchema)]
     #[serde(crate = "near_sdk::serde")]
     pub struct U256(4);
 }
+
+impl_uint_serde!(U256, 4);
