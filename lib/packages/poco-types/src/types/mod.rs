b00260a3f9f602beaa4c5756693df7914e5a1568
Twiliness <DarkHighness@users.noreply.github.com>
2023-02-01 13:33:21 +0800

    feat: 一组用于在不同 AccountId 类型转换的工具函数

--- lib/packages/poco-types/src/types/mod.rs
+++ lib/packages/poco-types/src/types/mod.rs
@@ -3,3 +3,15 @@ pub mod round;
 pub mod task;
 pub mod uint;
 pub mod user;
+
+pub fn convert_account_id_from_sdk_to_primitives(
+    account_id: &near_sdk::AccountId,
+) -> near_primitives::types::AccountId {
+    account_id.as_str().parse().unwrap()
+}
+
+pub fn convert_account_id_from_primitives_to_sdk(
+    account_id: &near_primitives::types::AccountId,
+) -> near_sdk::AccountId {
+    account_id.as_str().parse().unwrap()
+}
