f6c7d1391e2c2a3295e098fd1f6a82544771ebeb
wuoody <wuuudiii@qq.com>
2024-04-10 12:07:08 +0000

    [types]: move account_id utils to agent

--- lib/packages/poco-agent/src/util/mod.rs
+++ lib/packages/poco-agent/src/util/mod.rs
@@ -3,3 +3,15 @@ pub use misc::either_of;
 
 mod format;
 mod misc;
+
+pub fn convert_account_id_from_sdk_to_primitives(
+    account_id: &near_sdk::AccountId,
+) -> near_primitives::types::AccountId {
+    account_id.as_str().parse().unwrap()
+}
+
+pub fn convert_account_id_from_primitives_to_sdk(
+    account_id: &near_primitives::types::AccountId,
+) -> near_sdk::AccountId {
+    account_id.as_str().parse().unwrap()
+}
--- lib/packages/poco-types/src/types/mod.rs
+++ lib/packages/poco-types/src/types/mod.rs
@@ -4,16 +4,19 @@ pub mod task;
 pub mod uint;
 pub mod user;
 
-#[cfg(feature = "all")]
-pub fn convert_account_id_from_sdk_to_primitives(
-    account_id: &near_sdk::AccountId,
-) -> near_primitives::types::AccountId {
-    account_id.as_str().parse().unwrap()
-}
+// extern crate near_sdk;
+// extern crate near_primitives;
 
-#[cfg(feature = "all")]
-pub fn convert_account_id_from_primitives_to_sdk(
-    account_id: &near_primitives::types::AccountId,
-) -> near_sdk::AccountId {
-    account_id.as_str().parse().unwrap()
-}
+
+
+// pub fn convert_account_id_from_sdk_to_primitives(
+//     account_id: &near_sdk::AccountId,
+// ) -> near_primitives::types::AccountId {
+//     account_id.as_str().parse().unwrap()
+// }
+
+// pub fn convert_account_id_from_primitives_to_sdk(
+//     account_id: &near_primitives::types::AccountId,
+// ) -> near_sdk::AccountId {
+//     account_id.as_str().parse().unwrap()
+// }
